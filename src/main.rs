4318a1fe3c6bb9c8cf9bc8961b28a47d1b878481
Cassin01 <koyojb@gmail.com>
2022-09-21 22:42:46 +0900

    fix: first commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use dialoguer::{theme::ColorfulTheme, FuzzySelect, Editor, Input};
+
+fn main() {
+    let selections = &[
+        "fix: ",
+        "feat: ",
+        "BREAKING CHANGE: ",
+        "build:",
+        "chore: ",
+        "ci: ",
+        "docs: ",
+        "style: ",
+        "refactor: ",
+        "perf: ",
+        "test:",
+    ];
+
+    let selection = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .with_prompt("Pick your flavor")
+        .default(0)
+        .items(&selections[..])
+        .interact()
+        .unwrap();
+
+    let input: String = Input::with_theme(&ColorfulTheme::default())
+        .with_prompt(selections[selection])
+        .interact_text()
+        .unwrap();
+    println!("{}{}", selections[selection], input);
+}
