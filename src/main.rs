de5d7c46eca3b53d7dca8950d23771754fd0f487
Cassin01 <koyojb@gmail.com>
2022-09-22 20:46:47 +0900

    feat: add configuration

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,15 @@ extern crate serde_derive;
 use serde_derive::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Conf<'a> {
-     #[serde(borrow)]
-    types: Vec<Type<'a>>,
-    emoji: bool,
+struct Conf {
+    format: String,
+    types: Vec<Type>,
 }
 
-impl Default for Conf<'_> {
+impl Default for Conf {
     fn default() -> Self {
         Conf {
+            format: "{type}: {subject}".to_string(),
             types: vec![
                 Type::new("fix", "A bug fix", "🐛"),
                 Type::new("feat", "A new feature", "✨"),
@@ -25,42 +25,98 @@ impl Default for Conf<'_> {
                 Type::new("perf", " A code change that improves performance", "🚄"),
                 Type::new("test", "Adding or correcting tests", "🧪"),
             ],
-            emoji: false,
         }
     }
 }
 
-#[allow(dead_code)]
 #[derive(Debug, Serialize, Deserialize)]
-struct Type<'a> {
-    description: &'a str,
-    value: &'a str,
-    emoji: &'a str,
+struct Type {
+    description: String,
+    value: String,
+    emoji: String
 }
 
-impl<'a> Type<'a> {
-    fn new(value: &'a str, description: &'a str, emoji: &'a str) -> Self {
+impl Type {
+    fn new(value: &str, description: &str, emoji: &str) -> Self {
         Self {
-            description,
-            value,
-            emoji,
+            description: description.to_string(),
+            value: value.to_string(),
+            emoji: emoji.to_string(),
         }
     }
 }
 
-fn main() {
-    let types = &[
-        Type::new("fix", "A bug fix", "🐛"),
-        Type::new("feat", "A new feature", "✨"),
-        Type::new("BREAKING CHANGE", "Changes that introduces a breaking API change", "💥"),
-        Type::new("chore", "build system or external dependencies", "🛠️"),
-        Type::new("ci", "CI related changes", "💫"),
-        Type::new("docs", "Documentation only changes", "✏️"),
-        Type::new("style", "Changes that do not affect the meaning of the code", "💄"),
-        Type::new("refactor", "A code change that neither fixes a bug nor adds a feature", "🧹"),
-        Type::new("perf", " A code change that improves performance", "🚄"),
-        Type::new("test", "Adding or correcting tests", "🧪"),
-    ];
+fn inline(name: &str, index: usize, types: &Vec<Type>) -> String {
+    match name {
+        "emoji" =>types[index].emoji.clone(),
+        "type" => types[index].value.clone(),
+        "subject" =>  {
+            let input: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt(format!("{}", "subject"))
+                .interact_text().unwrap();
+            input
+        }
+        "body" => {
+            let input: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt(format!("{}", "body"))
+                .interact_text().unwrap();
+            input
+        },
+        "footer" => {
+            let input: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt(format!("{}", "footer"))
+                .interact_text().unwrap();
+            input
+        },
+        "scope" => {
+            let input: String = Input::with_theme(&ColorfulTheme::default())
+                .with_prompt(format!("{}", "scope"))
+                .interact_text().unwrap();
+            let ret = "(".to_owned() + &input + ")";
+            ret
+        }
+        _ => "".to_string()
+    }
+}
+
+fn p_syn(line: &str, index: usize, types: &Vec<Type>) -> Result<String, String> {
+    let mut cs = line.chars();
+    let mut ret = String::new();
+    loop {
+        if let Some(c) = cs.next() {
+            if c == '{' {
+                let mut r = String::new();
+                loop {
+                    if let Some(c) = cs.next() {
+                        if c == '}' {
+                            ret.push_str(&inline(&r, index, types));
+                            break;
+                        } else {
+                            r.push(c);
+                        }
+                    } else {
+                        return Err("There is no right bract".to_string());
+                    }
+                }
+            } else {
+                ret.push(c);
+            }
+        } else {
+            return Ok(ret);
+        }
+    }
+}
+
+fn parser(format: String, index: usize, types: &Vec<Type>) -> String {
+    let v : Vec<_> = format.split('\n').collect();
+    v.into_iter().fold(String::new(), |mut sum, x| {sum.push('\n'); sum.push_str(&p_syn(x, index, types).unwrap()); sum} )
+}
+
+type Res<T> = Result<T, Box<dyn std::error::Error>>;
+
+fn core() -> Res<()> {
+    let cfg: Conf = confy::load("rcz", None)?;
+    let types = &cfg.types;
     let selections: Vec<String> = types
         .into_iter()
         .map(|x| format!("{}: {}", x.value.clone(), x.description.clone()))
@@ -70,12 +126,16 @@ fn main() {
         .with_prompt("Pick your flavor")
         .default(0)
         .items(&selections[..])
-        .interact()
-        .unwrap();
+        .interact()?;
 
-    let input: String = Input::with_theme(&ColorfulTheme::default())
-        .with_prompt(format!("{}", types[selection].value))
-        .interact_text()
-        .unwrap();
-    println!("{}: {}", types[selection].value, input);
+
+    let format = parser(cfg.format, selection, &cfg.types);
+    println!("{}", format);
+    Ok(())
+}
+
+
+fn main() -> Res<()>{
+    core()?;
+    Ok(())
 }
